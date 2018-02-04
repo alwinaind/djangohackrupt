0db1317c098d7d0533c39d0d3e28a1357e45e783
alwinaind <alwinaind@gmail.com>
2018-02-04 13:37:58 +0530

    Added Backend

--- hack/views.py
+++ hack/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response,render, redirect
 from hack.forms import Industry
 from keras.models import model_from_json
+from sklearn.externals import joblib
 
 def home(request):
     if request.method == 'POST':
@@ -9,7 +10,11 @@ def home(request):
         if form.is_valid():
             choice = form.cleaned_data.get('choice')
         print(choice)
+
     else:
         form = Industry()
 
     return render(request, "index.html", {'form': form})
+
+def index(request):
+    return(render(request, "indexold.html"))
